9d1b5e7475fa3619bc6cee90c503f0cbdffe9c3f
Donal <osulld13@tcd.ie>
2015-12-27 21:29:08 +0000

    Added Parse.com link

--- app/src/main/java/com/example/ribbit2/Ribbit2Application.java
+++ app/src/main/java/com/example/ribbit2/Ribbit2Application.java
@@ -1,7 +1,30 @@
 package com.example.ribbit2;
 
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 /**
  * Created by Donal on 27/12/2015.
  */
-public class Ribbit2Application {
+public class Ribbit2Application extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /*
+            This is code tests the parse.com backend.
+         */
+
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/android/guide#local-datastore
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this);
+
+        //ParseObject testObject = new ParseObject("TestObject");
+        //testObject.put("foo", "bar");
+        //testObject.saveInBackground();
+    }
+
 }
